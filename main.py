c7fe495b0e23ac306d7b9a5bf93fb57ce7c84cbc
Petar Cevriz <pcevriz95@gmail.com>
2026-08-11 17:00:27 +0200

    Try out google auth package

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-"""
-# NOTE
-
-Looks like it is possible to get access token and refresh token via GoogleOauthPlayground, capture that and use it for the auth piece
-"""
 import collections
 import requests
 import os
 
+import google.oauth2.credentials as google_auth
+
 GoogleSettings = collections.namedtuple('GoogleSettings', ['client_id', 'client_secret', 'access_token', 'refresh_token'])
 
-
 def main():
+    settings = GoogleSettings(os.environ.get("CLIENT_ID"), os.environ.get("CLIENT_SECRET"), os.environ.get("ACCESS_TOKEN"), os.environ.get('REFRESH_TOKEN'))
+    # TODO: Read google-auth.readthedocs.io/en/master/reference/google.oauth2.credentials.html, looks like it will not work like this
+    credentials = google_auth.Credentials(token='garbage_value', refresh_token=settings.refresh_token)
+    if not credentials.valid:
+        credentials.refresh()
 
-    # 1. Access environment variables
-    google_settings = GoogleSettings(os.environ.get("CLIENT_ID"), os.environ.get("CLIENT_SECRET"), os.environ.get("ACCESS_TOKEN"), os.environ.get('REFRESH_TOKEN'))
-    print(google_settings)
     response = requests.api.get(url='https://gmail.googleapis.com/gmail/v1/users/me/profile', headers={
-       'Authorization': f'Bearer {google_settings.access_token}'
+       'Authorization': f'Bearer {credentials.token}'
     })
     print(f'Status Code: {response.status_code}')
-
     
-    # 2. Get access token and refresh token
-    # 3. Use access token to get the last email
 
 
 if __name__ == "__main__":
