2fd6deb9c58abbaecf40fcabe2988822fd93341b
Petar Cevriz <pcevriz95@gmail.com>
2026-08-11 13:03:39 +0200

    Do a simple test to check if I can get refresh and access token via Google Oauth playground just so I don't need to write code for it

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+"""
+# NOTE
+
+Looks like it is possible to get access token and refresh token via GoogleOauthPlayground, capture that and use it for the auth piece
+"""
+import collections
+import requests
+import os
+
+GoogleSettings = collections.namedtuple('GoogleSettings', ['client_id', 'client_secret', 'access_token'])
+
+
 def main():
-    print("Hello from e2sh-expense-monitor!")
+
+    # 1. Access environment variables
+    google_settings = GoogleSettings(os.environ.get("CLIENT_ID"), os.environ.get("CLIENT_SECRET"), os.environ.get("ACCESS_TOKEN"))
+    print(google_settings)
+    response = requests.api.get(url='https://gmail.googleapis.com/gmail/v1/users/me/profile', headers={
+       'Authorization': f'Bearer {google_settings.access_token}'
+    })
+    print(f'Status Code: {response.status_code}')
+
+    
+    # 2. Get access token and refresh token
+    # 3. Use access token to get the last email
 
 
 if __name__ == "__main__":
